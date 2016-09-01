b8f671d5fc2765e83298aaf0ccd973cfeca42fc3
gstefanoni <giorgio.stefanoni@gmail.com>
2016-09-01 17:45:55 +0100

    Adds a method for computing the sum of two binary numbers encoded using int arrays

--- Algos/src/com/github/gstef/algos/util/Arrays.java
+++ Algos/src/com/github/gstef/algos/util/Arrays.java
@@ -29,5 +29,24 @@ public class Arrays {
 		return -1;
 	}
 	
+	/**
+	 * Computes the addition of the  binary numbers encoded in the two arrays. The arrays
+	 * encode binary numbers from the least to the most significant bits.
+	 * @param a
+	 * @param b
+	 * @return An array c such that binary(c) = binary(a) + binary(b).
+	 */
+	public static int[] binaryAddition(int[] a, int[] b) {
+		assert a != null && b != null && a.length == b.length;
+		int[] sum = new int[a.length+1];
+		for (int i = 0; i < a.length; i++) {
+			int partialSum = a[i] + b[i] + sum[i];
+			// computing the least significant bit of the sum
+			sum[i] = partialSum % 2; 
+			// computing the carry
+			sum[i+1] = partialSum / 2; 
+		}
+		return sum;
+	}
 
 }
