dad682b6c1b6f811a6cd28f97700a5f010ef7c27
gstefanoni <giorgio.stefanoni@gmail.com>
2016-11-20 16:05:01 +0000

    Implemented merge sort

--- Algos/src/com/github/gstef/algos/util/ArraysUtil.java
+++ Algos/src/com/github/gstef/algos/util/ArraysUtil.java
@@ -1,5 +1,6 @@
 package com.github.gstef.algos.util;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class ArraysUtil {
@@ -78,6 +79,61 @@ public class ArraysUtil {
 			a[j+1] = key;
 		}
 	}
+	
+	
+	
+	/**
+	 * Sorts the given integer array using the mergesort
+	 * algorithm
+	 * @param a not-null integer array
+	 */
+	public static void mergeSort(int[] a) {
+		assert a != null;		
+		int[] b = Arrays.copyOf(a, a.length);
+		mergeSort(b, 0, a.length -1,a);
+	}
+	
+	/**
+	 * Auxiliary procedure for sorting an integer array b. The resulting
+	 * sorted array is stored in a.
+	 * @param b
+	 * @param start
+	 * @param end
+	 * @param a
+	 */
+	protected static void mergeSort(int[] b, int start, int end, int[] a) {
+		if (end > start) {	
+			int midpoint = (start + end)/2;
+			mergeSort(a, start, midpoint,b);
+			mergeSort(a, midpoint +1, end,b);
+			merge(b, start, midpoint, end,a);
+		}
+	}
+	
+	/**
+	 * Merging two adjacent sorted subsequences a[p..q] and a[q+1..r]
+	 * into a unique sorted sequence b[p..r].
+	 * @param a
+	 * @param p
+	 * @param q
+	 * @param r
+	 * @param b
+	 */
+	public static void merge(int[] a, int p, int q, int r, int[] b) {
+		assert p <= q && q <= r && r < a.length;
+		int i = p; // index on the left array a[p..q]
+		int j = q+1; // index on the right array a[q+1..r]
+		for (int k = p; k <= r; k++) {
+			if (i <= q && (j > r || a[i] <= a[j])) {
+				b[k] = a[i];
+				i++;
+			} else {
+				b[k] = a[j];
+				j++;
+			}
+		}
+	}
+	
 	
 	public static int[] randomIntArray(int[] array) {
 		return randomIntArray(array, new Random());
--- Algos/test/com/github/gstef/algos/util/TestArraySorting.java
+++ Algos/test/com/github/gstef/algos/util/TestArraySorting.java
@@ -1,5 +1,6 @@
 package com.github.gstef.algos.util;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import org.junit.Rule;
@@ -10,7 +11,7 @@ import junit.framework.TestCase;
 
 public class TestArraySorting extends TestCase {
 	
-	final int ARRAY_SIZE = 1000;
+	final int ARRAY_SIZE = 100;
 	final long SEED = 3514514561L;
 
 	
@@ -31,9 +32,7 @@ public class TestArraySorting extends TestCase {
 		for (int k = 0; k < 1000; k++) {
 			ArraysUtil.randomIntArray(array, generator);
 			ArraysUtil.insertionSort(array);
-			for (int i = 1; i < array.length; i++) {
-				assertTrue(array[i-1] <= array[i]);
-			}
+			assertTrue(isSorted(array));
 		}
 	}
 	
@@ -43,9 +42,43 @@ public class TestArraySorting extends TestCase {
 		for (int k = 0; k < 1000; k++) {
 			ArraysUtil.randomIntArray(array, generator);
 			ArraysUtil.selectionSort(array);
-			for (int i = 1; i < array.length; i++) {
-				assertTrue(array[i-1] <= array[i]);
-			}
+			assertTrue(isSorted(array));
 		}
+	}
+	
+	public void testmergeSort() {
+		int[] array = new int[ARRAY_SIZE];
+		Random generator = new Random(123135);
+		for (int k = 0; k < 1000; k++) {
+			ArraysUtil.randomIntArray(array, generator);
+			ArraysUtil.mergeSort(array);
+			assertTrue(isSorted(array));
+		}
+	}
+	
+	public void testMerge() {
+		int[] firstArray = new int[ARRAY_SIZE];
+		int[] secondArray = new int[ARRAY_SIZE];
+		int[] array;
+		int[] temp = new int[ARRAY_SIZE*2];
+		Random generator = new Random(123135);
+		for (int k = 0; k < 1000; k++) {
+			ArraysUtil.randomIntArray(firstArray, generator);
+			ArraysUtil.randomIntArray(secondArray, generator);
+			ArraysUtil.insertionSort(firstArray);
+			ArraysUtil.insertionSort(secondArray);
+			array = Arrays.copyOf(firstArray, ARRAY_SIZE *2);
+			System.arraycopy(secondArray, 0, array, ARRAY_SIZE, ARRAY_SIZE);
+			ArraysUtil.merge(array, 0, ARRAY_SIZE-1,  ARRAY_SIZE*2-1,temp);
+			assertTrue(isSorted(temp));
+		}
+	}
+	
+	protected boolean isSorted(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			if(array[i-1] > array[i])
+				return false;
+		}
+		return true;
 	}
 }
